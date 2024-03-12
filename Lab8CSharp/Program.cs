cea8b7fba605321905cadf8f30076bbb2e296414
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2024-03-12 19:59:27 +0200

    created main menu

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -8,10 +8,37 @@ class Program
 {
     public static void Main(string[] args)
     {
-        //Task1.Program.Main_Task1();
-        //Task2.Program.Main_Task2();
-        //Task3.Program.Main_Task3();
-        //Task4.Program.Main_Task4();
-        Task5.Program.Main_Task5();
+        while(true)
+        {
+            Console.WriteLine("Select the task number(1-5): ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Task1.Program.Main_Task1();
+                    break;
+
+                case 2:
+                    Task2.Program.Main_Task2();
+                    break;
+                
+                case 3:
+                    Task3.Program.Main_Task3();
+                    break;
+
+                case 4:
+                    Task4.Program.Main_Task4();
+                    break;
+                
+                case 5:
+                    Task5.Program.Main_Task5();
+                    break;
+
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
     }
 }
